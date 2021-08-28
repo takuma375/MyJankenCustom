b3938995a68db08a9f31a11b1335b23ae73be320
takuma375 <takuma-shimizu-10174@keio.jp>
2021-08-29 02:55:59 +0900

    Update 手の数字から、出した手の名前や画像名を表示する関数を追加した
    数値で管理されている手を、実際の画像名や手の名前に変換することで今後の処理が簡単になると予想。。。

--- MyJankenCustom/ViewController.swift
+++ MyJankenCustom/ViewController.swift
@@ -28,14 +28,31 @@ class ViewController: UIViewController {
             newSystemHandNumber = Int.random(in: 0..<3)
         } while systemHandNumber == newSystemHandNumber
         systemHandNumber = newSystemHandNumber
-        
-        if systemHandNumber == 0 {
-            systemHandImage.image = UIImage(named: "gu")
-        } else if systemHandNumber == 1 {
-            systemHandImage.image = UIImage(named: "choki")
-        } else if systemHandNumber == 2 {
-            systemHandImage.image = UIImage(named: "pa")
+    }
+    
+    // 手の数字から、手の画像名を取得する関数
+    func handNumberToHandImageName(handNumber: Int) -> String {
+        var handImageName = ""
+        if handNumber == 0 {
+            handImageName = "gu"
+        } else if handNumber == 1 {
+            handImageName = "choki"
+        } else if handNumber == 2 {
+            handImageName = "pa"
         }
+        return handImageName
+    }
+    // 手の数字から、手の名前を取得する関数
+    func handNumberToHandName(handNumber: Int) -> String {
+        var handName = ""
+        if handNumber == 0 {
+            handName = "グー"
+        } else if handNumber == 1 {
+            handName = "チョキ"
+        } else if handNumber == 2 {
+            handName = "パー"
+        }
+        return handName
     }
     
 }
