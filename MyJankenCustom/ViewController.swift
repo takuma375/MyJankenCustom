eb579ac24ebef034939066fcceed19a3f21c5351
takuma375 <takuma-shimizu-10174@keio.jp>
2021-08-29 02:32:18 +0900

    Update システムの出す手をランダムに決定する処理を追加。
    0〜2の間で乱数を生成し、じゃんけんの各手に対応させている。
    repeat-whileを使用し、新しい数値が前回の数値と同じであれば、乱数の生成を繰り返すという処理を追加していることで、同じ手が出ないようにしている。

--- MyJankenCustom/ViewController.swift
+++ MyJankenCustom/ViewController.swift
@@ -13,15 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerHandLabel: UILabel!
     @IBOutlet var playerHandsButton: [UIButton]!
     
+    // システムの手を記録する変数(0, 1, 2)
+    var systemHandNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func playerHandsTapped(_ sender: UIButton) {
-        resultLabel.text = "あなたの勝ちです"
-        systemHandImage.image = UIImage(named: "gu")
-        playerHandLabel.text = "パーを出しました"
+        var newSystemHandNumber: Int
+        // システムの出す次の手をランダムに生成(前の手と異なるものになるまで実行)
+        repeat {
+            newSystemHandNumber = Int.random(in: 0..<3)
+        } while systemHandNumber == newSystemHandNumber
+        systemHandNumber = newSystemHandNumber
+        
+        if systemHandNumber == 0 {
+            systemHandImage.image = UIImage(named: "gu")
+        } else if systemHandNumber == 1 {
+            systemHandImage.image = UIImage(named: "choki")
+        } else if systemHandNumber == 2 {
+            systemHandImage.image = UIImage(named: "pa")
+        }
     }
     
 }
