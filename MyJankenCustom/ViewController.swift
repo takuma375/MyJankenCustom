acfde050065bb5111aa94056363555bdf77a4125
takuma375 <takuma-shimizu-10174@keio.jp>
2021-08-29 01:16:14 +0900

    Update 各種UI要素の初期状態の定義とラベルやボタンに対してアウトレットとアクションを作成した

--- MyJankenCustom/ViewController.swift
+++ MyJankenCustom/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var systemHandImage: UIImageView!
+    @IBOutlet weak var playerHandLabel: UILabel!
+    @IBOutlet var playerHandsButton: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playerHandsTapped(_ sender: UIButton) {
+        resultLabel.text = "あなたの勝ちです"
+        systemHandImage.image = UIImage(named: "gu")
+        playerHandLabel.text = "パーを出しました"
+    }
+    
 }
 
